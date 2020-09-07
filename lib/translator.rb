14465e087e6bf452f424eaa48067116b01d954e9
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-07 22:14:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,6 @@ def get_english_meaning(path, emoticon)
   return "Sorry, that emoticon was not found"
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
 end
