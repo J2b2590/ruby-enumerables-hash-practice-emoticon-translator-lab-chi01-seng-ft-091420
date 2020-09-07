b911e9883051e9218d6f675b2d1e81fddf0efb0f
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-07 22:00:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,12 @@
 # require modules here
-# require "pry"
-require 'yaml'
-
+require "pry"
+require "yaml"
 
 def load_library(path)
+
   final_hash = {}
-  YAML.load_file(path).each do |key, value|
-    final_hash[key] = {}
-  end
-  final_hash
+  # code goes here
 end
-
-
-
-
 
 def get_japanese_emoticon
   # code goes here
